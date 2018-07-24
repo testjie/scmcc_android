64b3358085244bc7f125e170ca3f90c9001f1271
snake <snake_goodluck@hotmail.com>
2018-07-24 17:08:57 +0800

    数据驱动v1.0

--- libs/unlock_phone.bat
+++
@@ -1,9 +0,0 @@
-adb shell echo "down"
-adb shell sendevent /dev/input/event0 1 116 1
-adb shell sendevent /dev/input/event0 0 0 0
-adb shell sendevent /dev/input/event0 1 116 0
-adb shell sendevent /dev/input/event0 0 0 0
-adb shell echo "up"
-adb shell echo "start_unlock"
-adb shell input touchscreen  swipe 600 1000 600 300
-adb shell echo "end_unlock"
